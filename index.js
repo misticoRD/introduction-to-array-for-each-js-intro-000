2badc6d3bdd135d8d040eaa4f76a294659c2a00b
Josia Rodriguez <josiarodriguez@gmail.com>
2019-09-15 15:00:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-// Add your doToElementsInArray() function here:
-var evens = [0, 2, 4, 6, 8, 10];
 
-evens.forEach(even => {
-  console.log(`${even} is not odd!`);
-});
-
-evens.forEach((even, index, array) => {
-  debugger;
-  console.log(`${even} is not odd!`);
-});
 
 function doToElementsInArray(array, callback) {
   array.forEach(callback);
